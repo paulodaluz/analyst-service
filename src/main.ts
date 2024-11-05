31921f4b72b98cf53b35837f32157df47d0877cc
Paulo da Luz <paulera.daluz@gmail.com>
2024-11-05 20:31:07 -0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import pjson from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Analyst API')
+    .setDescription('API for managing analysts')
+    .setVersion(pjson.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
