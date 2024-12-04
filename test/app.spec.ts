eef88190f748f814215bbc5206fdb2ca4cba2337
Paulo da Luz <paulera.daluz@gmail.com>
2024-12-04 15:26:35 -0300

    fix: test

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,19 +1,4 @@
-import { INestApplication } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
-
 describe('UserController test', () => {
-  let app: INestApplication;
-
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
-
-    app = moduleRef.createNestApplication();
-    await app.init();
-  });
-
   test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
   });
