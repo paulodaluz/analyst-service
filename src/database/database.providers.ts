3f3174bb0d75e39ca38c614d1de2f5c4195f6ed9
Paulo da Luz <paulera.daluz@gmail.com>
2025-01-14 20:33:00 -0300

    fix: databse url

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
       mongoose.connect(
-        'mongodb://mongo:osRfIQCMLCgIrKKIUHnlOnhTLyxJITDH@junction.proxy.rlwy.net:24693',
+        'mongodb://mongo:IXCcAYlGyVNxgHvtuSchJTdQvflXNzjI@junction.proxy.rlwy.net:36263',
       ),
   },
 ];
